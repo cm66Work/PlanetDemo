85260ba54a578000cf36a825d5bc5064d035745a
Connor Marshall <cm66.work@gmail.com>
2023-12-16 20:35:45 +0400

    +

---
+++ Game.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Game
+{
+public:
+	Game(); // creator
+	virtual ~Game(); // destructor
+};
+
--- main.cpp
+++ main.cpp
@@ -1,32 +1,46 @@
 #include <SFML/Graphics.hpp>
 
-
 int main()
 {
 	// Create the window.
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Planet Demo");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Planet Demo", sf::Style::Titlebar | sf::Style::Close);
 
 	// run the program as long as the window is open
 	while (window.isOpen())
 	{
-		// check all the window's events that where triggered since the last iteration of the loop
+		// input from the user or other areas of the game.
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// "close requested" event: we close the window.
-			if (sf::Event::Closed == event.type)
+			switch (event.type)
+			{
+				// "X" button pressed on the window
+			case sf::Event::Closed:
 				window.close();
+				break;
+			
+				// if "ESC" pressed, close the game.
+			case sf::Event::KeyPressed:
+				if (sf::Keyboard::Escape == event.key.code)
+				{
+					window.close();
+					break;
+				}
+			}
 		}
 
-		// clear the window with black colour
+		// update the game.
+
+		// render the new change to the game based on the updated data.
+		// clear the window.
 		window.clear(sf::Color::Black);
 
-		// draw everything here...
-		// window.draw(...);
+		// draw everything here.
 
-		// end the current frame
+		// end current frame.
 		window.display();
-	}
 
+	}
 	return 0;
 }
+
